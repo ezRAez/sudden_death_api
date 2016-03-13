2f46337bfdfa52768ec0782e75ae1f5d0f961a81
Ezra Raez <ezRAez10@gmail.com>
2016-03-12 16:56:53 -0800

    add chat model

---
+++ models/chat.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose'),
+    User = require('./user.js'),
+    Game = require('./game.js');
+
+var ChatSchema = new mongoose.Schema({
+  sender:   {
+              type:     mongoose.Schema.Types.ObjectId,
+              ref:      'User',
+              required: true
+            },
+  game:     {
+              type:     mongoose.Schema.Types.ObjectId,
+              ref:      'Game',
+              required: true
+            },
+  content:  { type: String, validate: [checkLength, "Messages must be shorter \
+                                                     than 180 characters."] },
+
+});
+
+function checkLength(str) {
+  return str.length > 0 && str.length < 180;
+}
+
+
+var Chat = mongoose.model('Chat', ChatSchema);
+
+module.exports = Chat;
