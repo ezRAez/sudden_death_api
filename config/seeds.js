1742ea2be959751e9a97d725185407c3de07c3f8
Ezra Raez <ezRAez10@gmail.com>
2016-07-16 21:07:33 -0700

    draft 1 of seeds

--- config/seeds.js
+++ config/seeds.js
@@ -2,6 +2,7 @@ var mongoose = require('./database');
 
 var User = require('../models/user');
 var Spot = require('../models/spot');
+var Game = require('../models/game');
 
 var newUsers = [
   { // 0
@@ -90,27 +91,102 @@ var newSpots = [
       latlong: [34.115896, -118.233634],
       address: "3650 Verdugo Rd., Los Angeles, CA 90065",
       indoor:  false
-    },
+    }
 ];
 
 
-
-User.remove({}, function(err) {
+Game.remove({}, function(err) {
   if (err) console.log(err);
-  Spot.remove({}, function (err) {
+  User.remove({}, function(err) {
     if (err) console.log(err);
-    Spot.create(newSpots, function(err, spots) {
+    Spot.remove({}, function (err) {
       if (err) console.log(err);
+      Spot.create(newSpots, function(err, spots) {
+        if (err) console.log(err);
+        User.create(newUsers, function(err, users) {
+          if (err) console.log(err);
 
-      User.create(newUsers, function(err, users) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Database seeded with " + users.length  + " users.");
-          mongoose.connection.close();
-        }
-        process.exit();
-      });
-    });
-  });
-});
+          var newGames = [
+            { // 0
+              player1:   users[1],
+              player2:   users[0],
+              status:    "complete",
+              spot:      spots[0],
+              time:      new Date(2016, 4, 17),
+              winner_id: users[0],
+              p1rating:  [
+                {
+                  insideO:       77,
+                  outsideO:      45,
+                  defense:       67,
+                  sportsmanship: 100,
+                  comment:       "Such a nice dude. He burns you on those threes though."
+                }
+              ],
+              p2rating:  [
+                {
+                  insideO:       65,
+                  outsideO:      24,
+                  defense:       35,
+                  sportsmanship: 100,
+                  comment:       "Needs to work on his outside defense. Leaves you open for three."
+                }
+              ]
+            },
+            { // 1
+              player1:   users[1],
+              player2:   users[2],
+              status:    "accepted",
+              spot:      spots[1],
+              time:      new Date(2016, 9, 22)
+            },
+            { // 2
+              player1:   users[3],
+              player2:   users[4],
+              status:    "rejected"
+            },
+            { // 3
+              player1:   users[5],
+              player2:   users[4],
+              status:    "pending"
+            },
+            { // 3
+              player1:   users[5],
+              player2:   users[2],
+              status:    "complete",
+              spot:      spots[2],
+              time:      new Date(2016, 5, 15),
+              winner_id: users[5],
+              p1rating:  [
+                {
+                  insideO:       50,
+                  outsideO:      88,
+                  defense:       55,
+                  sportsmanship: 75,
+                  comment:       "She's really good. Made me feel bad about myself."
+                }
+              ],
+              p2rating:  [
+                {
+                  insideO:       45,
+                  outsideO:      10,
+                  defense:       20,
+                  sportsmanship: 80,
+                  comment:       "He's a lil punk. Dropped him, easy."
+                }
+              ]
+            }
+          ];
+
+          Game.create(newGames, function(err, games) {
+            if (err) console.log(err);
+
+            console.log(`Database seeded with ${users.length} users, ${games.length} games, and ${spots.length} spots.`);
+            mongoose.connection.close();
+            process.exit();
+          }); // Closes Game Create
+        }); // Closes User Create
+      }); // Closes Spot Create
+    }); // Closes Spot Remove
+  }); // Closes User Remove
+}); // Closes Game Remove
